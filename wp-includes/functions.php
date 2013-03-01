cf017b35379f78b10235ab1782f2bbbc456569ef
Rolf Kaiser <rolf_kaiser@yahoo.com>
2013-02-28 19:00:09 -0800

    Re-merge EKS hacks to the Wordpress core in wp_debug_backtrace_summary() to provide more verbose query logging.  This is an optional hack, that can be safely skipped.

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -3767,25 +3767,30 @@ function wp_debug_backtrace_summary( $ignore_class = null, $skip_frames = 0, $pr
 	$skip_frames++; // skip this function
 
 	foreach ( $trace as $call ) {
+            
+            // Start EKS hack 
+            $file = empty($call['file']) ? '' : $call['file'] . ':' . $call['line'] . " ";       
+            
 		if ( $skip_frames > 0 ) {
 			$skip_frames--;
 		} elseif ( isset( $call['class'] ) ) {
 			if ( $check_class && $ignore_class == $call['class'] )
 				continue; // Filter out calls
 
-			$caller[] = "{$call['class']}{$call['type']}{$call['function']}";
+			$caller[] = $file . "{$call['class']}{$call['type']}{$call['function']}";
 		} else {
 			if ( in_array( $call['function'], array( 'do_action', 'apply_filters' ) ) ) {
-				$caller[] = "{$call['function']}('{$call['args'][0]}')";
+				$caller[] = $file . "{$call['function']}('{$call['args'][0]}')";
 			} elseif ( in_array( $call['function'], array( 'include', 'include_once', 'require', 'require_once' ) ) ) {
-				$caller[] = $call['function'] . "('" . str_replace( array( WP_CONTENT_DIR, ABSPATH ) , '', $call['args'][0] ) . "')";
+				$caller[] = $file . $call['function'] . "('" . str_replace( array( WP_CONTENT_DIR, ABSPATH ) , '', $call['args'][0] ) . "')";
 			} else {
-				$caller[] = $call['function'];
+				$caller[] = $file . $call['function'] . '()';
 			}
 		}
 	}
 	if ( $pretty )
-		return join( ', ', array_reverse( $caller ) );
+		return join( '<br/>', array_reverse( $caller ) );
+                // END EKS hack
 	else
 		return $caller;
 }
