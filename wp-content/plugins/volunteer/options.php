71808423e796627bbe87dfdc7d54b69699d17c0b
Rolf Kaiser <rolf_kaiser@yahoo.com>
2013-07-08 14:46:45 -0700

    Provide county-specific email addresses to be notified when tax sites are added or updated.

--- wp-content/plugins/volunteer/options.php
+++ wp-content/plugins/volunteer/options.php
@@ -9,6 +9,19 @@ function eks_register_mysettings() {
     register_setting('eks-settings-group', 'terms_greeter');
     register_setting('eks-settings-group', 'terms_interpreter');
     register_setting('eks-settings-group', 'terms_screener');
+    
+    $counties = get_categories(
+                        array(
+                            'taxonomy' => VA_LISTING_CATEGORY,
+                            'name' => VA_LISTING_CATEGORY,
+                            'hide_empty' => false,
+                            'orderby' => 'name'
+                            ));
+    foreach($counties as $el)
+    {
+        $key = 'email_notifications_' . $el->term_id;
+        register_setting('eks-settings-group', $key);
+    }
 }
 
 function eks_settings_page() {
@@ -40,6 +53,29 @@ function eks_settings_page() {
         <p>Manage the EarnItKeepItSaveIt site settings below.</p>
 
         <form class="options" method="post" action="options.php">
+         
+            <fieldset>
+                <legend>Tax Site Email Notifications</legend>
+                <p>These people will be notified when Tax Sites are added or updated in the given county.  Enter addresses in the form of a comma-separated list.</p>
+                <?php $counties = get_categories(
+                        array(
+                            'taxonomy' => VA_LISTING_CATEGORY,
+                            'name' => VA_LISTING_CATEGORY,
+                            'hide_empty' => false,
+                            'orderby' => 'name'
+                            ));
+
+                foreach($counties as $el)
+                {
+                    $key = 'email_notifications_' . $el->term_id;
+                    ?>
+                    <div class="label"><?php echo $el->name ?></div>
+                    <input type="text" name="<?php echo $key ?>" style="width:100%" value="<?php echo get_option($key); ?>" placeholder="Enter a comma-separated list of email addresses" />
+                <?php
+                }
+                ?>
+            </fieldset>
+            
             <fieldset>
                 <legend>Volunteer Registration Email</legend>
                 <p>This is the content of the email that new volunteers and coordinators will receive upon registration.  For best results, only use plain text.  The special tokens <code>[USERNAME]</code> and <code>[PASSWORD]</code> will be replaced with the user's details when the email is sent.</p>
@@ -49,7 +85,9 @@ function eks_settings_page() {
                 <input type="text" name="volunteer_email_subject" style="width:50%" value="<?php echo get_option('volunteer_email_subject'); ?>" />
 
                 <div class="label">Email Body</div>
-                <?php wp_editor(get_option('volunteer_email_body'), 'volunteer_email_body'); ?>
+                <?php wp_editor(get_option('volunteer_email_body'), 'volunteer_email_body', array(
+                    'media_buttons' => false
+                )); ?>
             </fieldset>   
             
             <fieldset>
@@ -57,16 +95,24 @@ function eks_settings_page() {
                 <p>This is the copy that new volunteers will view during the registration process.</p>
 
                 <div class="label">Tax Preparer</div>
-                <?php wp_editor(get_option('terms_preparer'), 'terms_preparer'); ?>
+                <?php wp_editor(get_option('terms_preparer'), 'terms_preparer', array(
+                    'media_buttons' => false
+                )); ?>
                 
                 <div class="label">Greeter</div>
-                <?php wp_editor(get_option('terms_greeter'), 'terms_greeter'); ?>
+                <?php wp_editor(get_option('terms_greeter'), 'terms_greeter', array(
+                    'media_buttons' => false
+                )); ?>
                                 
                 <div class="label">Interpreter</div>
-                <?php wp_editor(get_option('terms_interpreter'), 'terms_interpreter'); ?>
+                <?php wp_editor(get_option('terms_interpreter'), 'terms_interpreter', array(
+                    'media_buttons' => false
+                )); ?>
                                                 
                 <div class="label">Screener</div>
-                <?php wp_editor(get_option('terms_screener'), 'terms_screener'); ?>
+                <?php wp_editor(get_option('terms_screener'), 'terms_screener', array(
+                    'media_buttons' => false
+                )); ?>
             </fieldset>   
             <?php submit_button(); ?>
 
