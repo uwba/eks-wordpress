e26e290c0d9472479b42c3205ae1bda4debbc578
Linda Shum <shum_design@hotmail.com>
2013-07-12 19:53:51 -0700

    updated styles and form for search in header.php, added images to listings page -- added text to label for cities instead of hiding since the select form is inside of a div now to compensate for Safari css display did some trickery but now it is fooling jQuery

--- wp-content/themes/eks/header-bak.php
+++ wp-content/themes/eks/header-bak.php
@@ -24,84 +24,59 @@
 </div>
 <div id="main-navigation" class="container">
   <div class="row">
-  <div id="rounded-nav-box" class="rounded">
-  <div id="rounded-nav-box-overlay">
-  <?php wp_nav_menu( array(
+    <div id="rounded-nav-box" class="rounded">
+      <div id="rounded-nav-box-overlay">
+      <?php wp_nav_menu( array(
 					'theme_location' => 'header',
 					'container_class' => 'menu rounded',
 					'items_wrap' => '<ul>%3$s</ul>',
 					'fallback_cb' => false,
 					'walker' => new ZG_Nav_Walker(),
 				) ); ?>
-  <?php if(is_post_type_archive('listing') || is_search()){ ?>
-  <!--****:::::::::::: start styled form :::::::::******-->
-  <div id="search-container">
-  <!-- //container for form-->
-  <h3>Choose your search terms</h3>
-  <!-- //Header for search box -->
-  <form id="searchform" method="get" action="<?php bloginfo( 'url' ); ?>">
-    <!-- ***** start form process when button is pressed ******** -->
-    <!-- <div id="main-search" class=""> left in comment for reference -->
-    <div class="formcolumn first">
-
-<p><label for="search-text">Search for keyword <span style="font-size:10px">(zip code, street or site name)</span></label></p>
- <input type="text" name="ls" id="search-text" class="text" value="<?php va_show_search_query_var('ls');?>" />
-
-<!-- // search for keyword -->
-<p><label for="select-language">Desired Language</label></p>
-<div class="styled-select"><select name="language" id="select-language">
-              <option></option>
-              <option>Spanish</option>
-              <option>Cantonese</option>
-              <option>Mandarin</option>
-              <option>Vietnamese</option>
-              <option>Khmer (Cambodian)</option>
-              <option>Farsi</option>
-              <option>American Sign Language</option>
-              <option>Russian</option>
-              <option>Tagalog</option>
-            </select>
-</div>
-<!-- // search for language -->
-</div><!-- // end formcolumn first -->
-
-<div class="formcolumn middle">
-<p><label for="select-itin">ITIN Applications Processed?</label></p>
-<div class="styled-select"><select name="itin" id="select-itin">
-              <option></option>
-              <option>Yes</option>
-              <option>No</option>
-            </select></div>
-<p><label for="select-ada">ADA Accessible?</label></p>
-<div class="styled-select"><select name="ada" id="select-ada">
-              <option></option>
-              <option>Yes</option>
-              <option>No</option>
-            </select></div>
-            
-</div> <!-- // end formcolumn middle --> 
-<div class="formcolumn last">
-
-<p><label for="select-county">County</label></p>
-<div class="styled-select"><select name="county" id="select-county">
-              <option></option>
-              <option>Alameda</option>
-              <option>Contra Costa</option>
-              <option>Marin</option>
-              <option>Napa</option>
-              <option>San Francisco</option>
-              <option>San Mateo</option>
-              <option>Solano</option>
-            </select></div>
-
-   <!-- //end id="cities" in case needed by jQuery -->         
-<div id="cities">
-<p><label for="select-city">City <span style="font-size:10px">(Choose your county first)</span></label></p>
-<div class="styled-select"><select name="city" id="select-city" width="120"><option></option>
-              <option>Alameda</option></select></div>
-<!-- //end county and city selects -->
-</div><!-- //end id="cities"  in case needed by jQuery  -->
-<script>
+      <?php if(is_post_type_archive('listing') || is_search()){ ?>
+      <form method="get" action="<?php bloginfo( 'url' ); ?>">
+        <div style="padding:10px">
+          <div id="main-search" class="yui3-g">
+            <div class="yui3-u-1-4">
+              <label for="search-text"> <span class="search-title">
+                <?php _e( 'Search For ', APP_TD ); ?>
+                </span><br/>
+                <span class="search-help">
+                <?php _e( '(e.g., zip code, street name, site name)', APP_TD ); ?>
+                </span> </label>
+              <div class="input-cont h39">
+                <div class="left h39"></div>
+                <div class="mid h39">
+                  <input type="text" name="ls" id="search-text" class="text" value="<?php va_show_search_query_var( 'ls' ); ?>" />
+                </div>
+                <div class="right h39"></div>
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-location" style="padding-left:10px">
+                <label> <span class="search-title">County</span> </label>
+                <div class="input-cont h39">
+                  <select name="county" id="select-county">
+                    <option></option>
+                    <option>Alameda</option>
+                    <option>Contra Costa</option>
+                    <option>Marin</option>
+                    <option>Napa</option>
+                    <option>San Francisco</option>
+                    <option>San Mateo</option>
+                    <option>Solano</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-location">
+                <label> <span class="search-title">City</span> </label>
+                <div class="input-cont h39">
+                  <select name="city" id="select-city">
+                  </select>
+                </div>
+                <script>
                 var firstrun = true;
         jQuery(document).ready(function($) {
             
@@ -223,34 +198,77 @@
 
         });
 </script>
-<div class="buttons">
-
-<div><button type="submit" id="search-submit" class="">Search</button>
-</div>  <!--//end button -->
-</div> <!--//end search formcolumn last -->
-</form><!--end form-->
-</div> <!-- // end search container -->
-
-  </div> <!--???--> 
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-button">
+                <!-- <input type="image" src="<?php echo get_bloginfo('template_directory'); ?>/images/search.png" value="<?php _e( 'Search', APP_TD ); ?>" /> -->
+                <button type="submit" id="search-submit" class="rounded-small">
+                <?php _e( 'Search', APP_TD ); ?>
+                </button>
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-location">
+                <label> <span class="search-title">Language</span> </label>
+                <div class="input-cont h39">
+                  <select name="language" id="select-language">
+                    <option></option>
+                    <option>Spanish</option>
+                    <option>Cantonese</option>
+                    <option>Mandarin</option>
+                    <option>Vietnamese</option>
+                    <option>Khmer (Cambodian)</option>
+                    <option>Farsi</option>
+                    <option>American Sign Language</option>
+                    <option>Russian</option>
+                    <option>Tagalog</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-location" style="padding-left:10px">
+                <label> <span class="search-title">ADA Accessible?</span> </label>
+                <div class="input-cont h39">
+                  <select name="ada" id="select-ada">
+                    <option></option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="yui3-u-1-4">
+              <div class="search-location">
+                <label> <span class="search-title">ITIN Applications Processed?</span> </label>
+                <div class="input-cont h39">
+                  <select name="itin" id="select-itin">
+                    <option></option>
+                    <option>Yes</option>
+                    <option>No</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <?php if ( '' != $orderby = va_get_search_query_var( 'orderby' )){ ?>
+        <input type="hidden" name="orderby" value="<?php echo $orderby; ?>" />
+        <?php } ?>
+        <?php if ( '' != $radius = va_get_search_query_var( 'radius' )){ ?>
+        <input type="hidden" name="radius" value="<?php echo $radius; ?>" />
+        <?php } ?>
+        <?php if ( isset( $_GET['listing_cat'] ) ){ ?>
+        <?php foreach ( $_GET['listing_cat'] as $k=>$listing_cat ) { ?>
+        <input type="hidden" name="listing_cat[]" value="<?php echo $listing_cat; ?>" />
+        <?php } ?>
+        <?php } ?>
+        </div>
+      </form>
+      <?php } ?>
     </div>
-    </div>
-    </div>
-    <?php if ( '' != $orderby = va_get_search_query_var( 'orderby' )){ ?>
-    <input type="hidden" name="orderby" value="<?php echo $orderby; ?>" />
-    <?php } ?>
-    <?php if ( '' != $radius = va_get_search_query_var( 'radius' )){ ?>
-    <input type="hidden" name="radius" value="<?php echo $radius; ?>" />
-    <?php } ?>
-    <?php if ( isset( $_GET['listing_cat'] ) ){ ?>
-    <?php foreach ( $_GET['listing_cat'] as $k=>$listing_cat ) { ?>
-    <input type="hidden" name="listing_cat[]" value="<?php echo $listing_cat; ?>" />
-    <?php } ?>
-    <?php } ?>
-    </div>
-  </form>
-  <?php } ?>
-</div>
-</div>
+  </div>
 </div>
 </div>
 <div id="breadcrumbs" class="container">
