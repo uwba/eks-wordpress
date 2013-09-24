7fef0b2ef77f38547687e81b8057b3235e9a9014
Linda Shum <lshum@uwba.org>
2013-09-24 12:05:14 -0700

    repair path of the image on the listings page

--- wp-content/themes/eks/archive-listing.php
+++ wp-content/themes/eks/archive-listing.php
@@ -205,7 +205,7 @@ while ( have_posts() ) {
 
 <?php } else { // First time you hit the page, prior to submitting a search query ?>
     
-    <article class="listing"><p>Search for a Tax Site above. Your results will appear here. </p></article><img class="taxsite-pic" src="http://eks.echothis.com/wp-content/uploads/2013/07/tax-site.jpg"  width="600" height="563" class="alignnone" />
+    <article class="listing"><p>Search for a Tax Site above. Your results will appear here. </p></article><img class="taxsite-pic" src="http://earnitkeepitsaveit.org/wp-content/uploads/2013/07/tax-site.jpg"  width="600" height="563" class="alignnone" />
     
 <?php } ?>
 </div>
